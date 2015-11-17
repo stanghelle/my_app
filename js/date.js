0cc678e7d4028146abce956347c1c40e80021a1d
Stanghelle <post@stanghelle.net>
2015-11-17 10:08:11 +0100

    add new js

---
+++ js/date.js
@@ -0,0 +1,21 @@
+<script type="text/javascript">
+function addZero(i) {
+    if (i < 10) {
+        i = "0" + i;
+    }
+    return i;
+}
+
+function SetDate()
+{
+var date = new Date();
+
+var min = addZero(date.getMinutes());
+var hour = addZero(date.getHours());
+
+var today =  hour +":"+min;
+
+
+document.getElementById('myDate').value = today;
+}
+</script>
---
+++ js/form.js
@@ -0,0 +1,20 @@
+$('form').submit(function(){
+	var landmarkID = $(this).parent().attr('data-landmark-id');
+	var postData = $(this).serialize();
+	
+	$.ajax({
+		type: 'POST',
+		data: postData+'&amp;lid='+landmarkID,
+		url: 'http://your-domain.com/comments/save.php',
+		success: function(data){
+			console.log(data);
+			alert('Your comment was successfully added');
+		},
+		error: function(){
+			console.log(data);
+			alert('There was an error adding your comment');
+		}
+	});
+	
+	return false;
+});
